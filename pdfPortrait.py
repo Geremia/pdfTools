4aa0859c8c0e4c9121741da03a96b2b655550730
Geremia <Geremia@users.noreply.github.com>
2025-12-22 15:36:39 -0700

    This script answers the https://askubuntu.com/q/1562136/362789 question.

---
+++ pdfPortrait.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import pymupdf as p
+import sys
+
+if len(sys.argv) < 3:
+    print("Usage: %s <input PDF> <output PDF>" % sys.argv[0])
+    sys.exit(1)
+
+pdf = sys.argv[1]
+outputPDF = sys.argv[2]
+
+try:
+    doc = p.open(pdf)
+except:
+    print("Error opening %s" % pdf)
+    sys.exit(1)
+
+for page in doc:
+    r = page.rect
+    if r.width > r.height:
+        print("Page %d: %d x %d is landscape. Rotating 90°." % (page.number, height, width))
+        page.set_rotation(90)
+
+doc.save(outputPDF)
